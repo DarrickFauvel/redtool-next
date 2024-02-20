0f26c419e8c115974bf2665bb775acf72309b509
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-02-20 10:25:05 -0500

    Add className prop

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -2,9 +2,10 @@ import { ReactNode } from "react"
 
 type ContainerProps = {
   children: ReactNode
+  className?: string
 }
 
-function Container({ children }: ContainerProps) {
-  return <div className="px-4 mx-auto">{children}</div>
+function Container({ children, className }: ContainerProps) {
+  return <div className={`px-4 mx-auto ${className}`}>{children}</div>
 }
 export default Container
