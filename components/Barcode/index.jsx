8aebff37bbd2df90406b4db903eee6787ea3a92f
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-02-26 03:41:02 -0500

    Update

--- components/Barcode/index.jsx
+++ components/Barcode/index.jsx
@@ -1,14 +1,8 @@
-// import { useStore } from '@nanostores/react';
-// import { generatorFormData } from '../../stores/barcodeGeneratorStore';
-
 import ReactBarcode from "react-barcode"
 
 const Barcode = (props) => {
-  // const $generatorFormData = useStore(generatorFormData)
-
   return (
     <ReactBarcode
-      // value={props.barcodeValue || $generatorFormData.barcodeValue}
       value={props.barcodeValue}
       height={50}
       text={props.dpci || undefined}
--- components/Qrcode/index.tsx
+++ components/Qrcode/index.tsx
@@ -4,10 +4,10 @@ import qrcodeImage from "@/assets/images/redtool-qrcode.svg"
 const Qrcode = () => (
   <>
     <span className="uppercase text-red-700 font-bold">Share this app</span>
-    <div className="flex flex-col gap-2 p-2 border-[1px] border-red-700 rounded-lg text-center text-red-700 uppercase text-sm">
+    <div className="flex flex-col gap-2 p-2 w-44 border-[1px] border-red-700 rounded-lg text-center text-red-700 uppercase text-sm">
       <Image src={qrcodeImage} alt="qrcode for redTool" priority />
       <span
-        className="text-sm lowercase text-gray-800
+        className="text-xs lowercase text-gray-800
       ">
         redtool.darrickdevelops.com
       </span>
