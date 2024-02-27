138c307b8dc96088bdddebff9043be8aeb1cf5a7
Darrick Fauvel <darrick.fauvel@gmail.com>
2024-02-27 11:06:18 -0500

    Remove router back and replace with absolute link

--- components/BackLink/index.tsx
+++ components/BackLink/index.tsx
@@ -1,21 +1,11 @@
-"use client"
 import Link from "next/link"
 import Icon from "@/components/Icon"
-import { useRouter } from "next/navigation"
 
 const BackLink = () => {
-  const router = useRouter()
-
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault()
-    router.back()
-  }
-
   return (
     <Link
       className="flex items-center font-bold text-lg no-underline mb-8 gap-1 hover:text-black"
-      href="#"
-      onClick={handleClick}>
+      href="/">
       <Icon icon="bi:arrow-left" />
       <span>Back</span>
     </Link>
